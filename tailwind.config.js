fb17125a3cc87c4fe91cbd38fa233bb2ec0ebaca
buyfakett <buyfakett@vip.qq.com>
2025-05-07 16:25:00 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.tsx',
+    './src/components/**/*.tsx',
+    './src/layouts/**/*.tsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
